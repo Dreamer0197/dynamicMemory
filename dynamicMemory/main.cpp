3d76dd377d2dc8871cc6f4f60ac0f3c1963e54b2
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-17 15:39:56 +0300

    Initial Commit

---
+++ dynamicMemory/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dynamicMemory
+//
+//  Created by Kaan Şengün on 17.11.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
